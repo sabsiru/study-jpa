baaed7c95727a7f1742a7ef8841da72fa29d27c6
sabsiru <zero013@naver.com>
2024-08-05 11:37:19 +0900

    엔티티속성만 상속하는 MapperSuperclass 추가사항

--- ex1-hello-jpa/src/main/java/hellojpa/BaseEntity.java
+++ ex1-hello-jpa/src/main/java/hellojpa/BaseEntity.java
@@ -10,4 +10,36 @@ public abstract class BaseEntity {
     private LocalDateTime createDate;
     private String updateBy;
     private LocalDateTime updateDate;
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
 }

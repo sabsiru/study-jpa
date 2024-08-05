99a606a2392069df89a818c38d46958efc2ef45d
sabsiru <zero013@naver.com>
2024-08-05 11:33:12 +0900

    엔티티속성만 상속하는 MapperSuperclass 추가사항
    매핑정보만 제공므로 조회, 검색이 불가하다.

--- ex1-hello-jpa/src/main/java/hellojpa/BaseEntity.java
+++ ex1-hello-jpa/src/main/java/hellojpa/BaseEntity.java
@@ -4,7 +4,8 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass //엔티티속성만 상속하는 어노테이션
-public class BaseEntity {
+//매핑정보만 제공하므로 조회, 검색(em.find)가 불가하다.
+public abstract class BaseEntity {
     private String createBy;
     private LocalDateTime createDate;
     private String updateBy;

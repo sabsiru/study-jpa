36021aadbcd4c161f22ec0f3b8349e364f7bfd3b
sabsiru <zero013@naver.com>
2024-08-10 22:11:32 +0900

    값 타입의 부작용을 방지 하기위해 불변 객체로 설계한다.

--- ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -4,16 +4,16 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class Address {
-    private String street;
     private String city;
+    private String street;
     private String zipcode;
 
     public Address() {
     }
 
-    public Address(String street, String city, String zipcode) {
-        this.street = street;
+    public Address (String city,String street, String zipcode) {
         this.city = city;
+        this.street = street;
         this.zipcode = zipcode;
     }
 
@@ -21,7 +21,7 @@ public class Address {
         return street;
     }
 
-    public void setStreet(String street) {
+    private void setStreet(String street) {
         this.street = street;
     }
 
@@ -29,7 +29,7 @@ public class Address {
         return city;
     }
 
-    public void setCity(String city) {
+    private void setCity(String city) {
         this.city = city;
     }
 
@@ -37,7 +37,7 @@ public class Address {
         return zipcode;
     }
 
-    public void setZipcode(String zipcode) {
+    private void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
 }
--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,12 +14,27 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city","street","10000"));
-            member.setWorkPeriod(new Period());
+            Address address = new Address("city", "street", "10000");
 
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            //값을 공유하면 같이 변경됨. 그러므로 객체를 복사해서 넣어야함
+            //Address address2 = new Address("city", "street", "10000");
+            member2.setHomeAddress(copyAddress);
+            em.persist(member2);
+
+            //불변객체로 설계해야함 setter를 private로 하거나 만들지않아서 원천 차단
+            //member.getHomeAddress().setCity("newCity");
+            //값을 바꾸고 싶다면 통으로 갈아 끼워야한다.
+            Address newAddress = new Address("NewCIty", address.getStreet(), address.getZipcode());
+            member2.setHomeAddress(newAddress);
 
             tx.commit();
         } catch (Exception e) {
